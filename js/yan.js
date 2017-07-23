8c5865f777b991e5d8e245e9d75865902f9b0fd1
yanniznik <account@yananas.com>
2017-07-23 23:50:49 +0300

    Added animations

--- js/yan.js
+++ js/yan.js
@@ -1,11 +1,27 @@
 $(document).ready( function() {
-    
 
-    	$(".iphone-container").on("click", ".nav", function() {
-        $("#content").load("html/"+ $(this).attr("data-nav") + ".html");
-    });
 
-    	 $("#content").load("html/authentification.html");
+	$(".iphone-container").on("click", ".nav", function() {
+		if ($(this).hasClass( "animate" )) {
+			$(".phone-screen").addClass('loading');
+			setTimeout(function(){
+			$('.phone-screen').removeClass('loading');
+		},1000);
+		}
+		
+		$("#content").load("html/"+ $(this).attr("data-nav") + ".html");
+		
+	});
+
+	setTimeout(function(){
+			$(".loader").fadeOut();
+		},1000);
+
+	
+
+	$("#content").load("html/authentification.html");
+
+
 
 
 
